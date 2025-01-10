da40be02796886791d048f8c362ddf327f3a5e01
wmira <warrenmira@gmail.com>
2025-01-10 17:24:26 +0800

    updated title

--- main/src/utils/AppConfig.ts
+++ main/src/utils/AppConfig.ts
@@ -1,7 +1,7 @@
 export const AppConfig = {
 	site_name: 'Miranext',
 	title: 'Miranext | Will Code for Food',
-	description: 'Musings and Other Stuff',
+	description: 'Miranext | Will Code for Food',
 	author: 'Warren Mira',
 	locale_region: 'de-en',
 	locale: 'en'
